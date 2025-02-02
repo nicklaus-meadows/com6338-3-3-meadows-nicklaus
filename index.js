b84854300b9d183ec861138d08df52f4da98f680
Nicklaus <nicklaus.meadows@ufl.edu>
2025-02-02 07:31:00 -0500

    adding the counter t vs f

--- index.js
+++ index.js
@@ -2,24 +2,29 @@
 
 function runQuiz() {
     var questionsArr = [
-       {question1: 'Javascript is the best language.', answer: true},
-       {question2: 'JS is slowly starting to click.', answer: true},
-       {question3: 'JS is fun to learn.', answer: true},
-       {question4: 'JS gives me a headache sometimes.', answer: true},
-       {question5: 'JS sometimes goes smooth, like maybe TOO SMOOTH.', answer: true},
-       {question6: 'Sometimes you wait for the impending doom of a code crash', answer: true}
+       {question: 'Javascript is the best language.', answer: true},
+       {question: 'JS is slowly starting to click.', answer: true},
+       {question: 'JS is fun to learn.', answer: true},
+       {question: 'JS gives me a headache sometimes.', answer: true},
+       {question: 'JS sometimes goes smooth, like maybe TOO SMOOTH.', answer: true},
+       {question: 'Sometimes you wait for the impending doom of a code crash', answer: true}
 
 
     ]
 
     var trueCount = 0
+    var falseCount = 0
 
     for (var i = 0; i < questionsArr.length; i++) {
-        var questions = questionsArr[i]
-        var answer = confirm(questions)
+        var question = questionsArr[i].question;
+        var answer = confirm(question)
         if (answer) {
             trueCount++
         }
+        else {
+            falseCount++
+        }
     }
     
+    alert('Number of Correct Answers ' + trueCount)
 }
